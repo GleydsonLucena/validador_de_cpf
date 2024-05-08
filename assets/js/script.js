669bf3f7125688b1f71540cac360de9ebd9aa5b9
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-05-08 18:51:32 -0300

    Atualização

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+class Validator {
+    constructor (cpf) {
+        this.cpf = cpf;
+    }
+    result = document.querySelector('.result-off');
+
+
+    openResult() {
+        if (this.result.style.display === 'block') return;
+        this.result.style.display = 'block';
+        setTimeout(e=> {
+            this.result.style.display = 'none';
+        }, 4000);
+    };
+
+
+    
+}
+
+const valid = new Validator('06957387127');
+
+
+document.addEventListener('click', 
+e => {
+    const el = e.target;
+    const btn = el.classList.contains('btn')
+    if (btn) valid.openResult();
+})
+
